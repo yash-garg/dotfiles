4f20658d5788b4356e584683f9b97d354ad64154
Yash-Garg <me@yashgarg.dev>
2025-03-06 15:55:12 +0530

    darwin: extend homebrew module

--- modules/darwin/homebrew/default.nix
+++ modules/darwin/homebrew/default.nix
@@ -1,9 +1,10 @@
 {
-  lib,
   config,
+  lib,
   namespace,
   ...
 }:
+with lib;
 with lib.${namespace};
 let
   casks = [
@@ -21,43 +22,69 @@ let
     "visual-studio-code"
     "zed"
   ];
+  cfg = config.${namespace}.homebrew;
   hmModules = lib.snowfall.fs.get-snowfall-file "modules/home";
 in
 {
-  homebrew = enabled // {
-    brews = [
-      "cocoapods"
-      "ruby"
-    ];
-
-    casks = map (cask: {
-      name = cask;
-      greedy = true;
-    }) casks;
-
-    caskArgs.appdir = "/Applications";
-
-    global = {
-      autoUpdate = true;
-      brewfile = true;
+  options.${namespace}.homebrew = {
+    additionalCasks = mkOption {
+      type = types.listOf types.string;
+      default = [ ];
+      description = ''
+        A list of additional casks to install.
+      '';
     };
 
-    taps = [ ];
-
-    masApps = { };
-
-    onActivation = {
-      autoUpdate = false;
-      cleanup = "zap";
-      upgrade = true;
+    masApps = mkOption {
+      type = types.attrsOf types.int;
+      default = { };
+      description = ''
+        A map of macOS App Store apps to install.
+      '';
     };
   };
 
-  # Since we aren't managing graphical apps with home-manager
-  # on darwin, add the config files directly in xdg config
-  snowfallorg.users.${config.${namespace}.user.name}.home.config = {
-    xdg.configFile = {
-      "ghostty/config".source = "${hmModules}/ghostty/config";
+  config = {
+    homebrew = enabled // {
+      brews = [
+        "cocoapods"
+        "ruby"
+      ];
+
+      casks =
+        map (cask: {
+          name = cask;
+          greedy = true;
+        }) casks
+        ++ map (cask: {
+          name = cask;
+          greedy = true;
+        }) cfg.additionalCasks;
+
+      caskArgs.appdir = "/Applications";
+
+      global = {
+        autoUpdate = true;
+        brewfile = true;
+      };
+
+      taps = [ ];
+
+      inherit (cfg) masApps;
+
+      onActivation = {
+        autoUpdate = false;
+        cleanup = "zap";
+        upgrade = true;
+      };
+    };
+
+    # Since we aren't managing graphical apps with home-manager
+    # on darwin, add the config files directly in xdg config
+    snowfallorg.users.${config.${namespace}.user.name}.home.config = {
+      xdg.configFile = {
+        "ghostty/config".source = "${hmModules}/ghostty/config";
+      };
     };
   };
 }
--- systems/aarch64-darwin/trinity/default.nix
+++ systems/aarch64-darwin/trinity/default.nix
@@ -1,4 +1,23 @@
 _: {
+  dots.homebrew = {
+    additionalCasks = [
+      "arc"
+      "chatgpt"
+      "discord"
+      "steam"
+    ];
+
+    masApps = {
+      Amphetamine = 937984704;
+      Bitwarden = 1352778147;
+      "Prime Video" = 545519333;
+      Tailscale = 1475387142;
+      Telegram = 747648890;
+      Unread = 1363637349;
+      WhatsApp = 310633997;
+    };
+  };
+
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = 4;
