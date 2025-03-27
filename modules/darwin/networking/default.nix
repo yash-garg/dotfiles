4032d7ea9312c8fcae938849e1bf2a7e91a15db8
Yash-Garg <me@yashgarg.dev>
2025-03-27 14:04:20 +0530

    darwin: remove custom dns servers

--- modules/darwin/networking/default.nix
+++ modules/darwin/networking/default.nix
@@ -1,11 +1,3 @@
 _: {
-  networking = {
-    knownNetworkServices = [ "Wi-Fi" ];
-    dns = [
-      "1.1.1.1"
-      "1.0.0.1"
-      "2606:4700:4700::1111"
-      "2606:4700:4700::1001"
-    ];
-  };
+  networking.knownNetworkServices = [ "Wi-Fi" ];
 }
