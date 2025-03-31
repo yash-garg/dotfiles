58c1d430b7355272e552e32d771dcab531eed6c7
Yash-Garg <me@yashgarg.dev>
2025-03-31 18:06:09 +0530

    fix: set btop color theme to tty

--- modules/home/btop/default.nix
+++ modules/home/btop/default.nix
@@ -7,6 +7,7 @@ with lib.${namespace};
 {
   programs.btop = enabled // {
     settings = {
+      color_theme = "TTY";
       theme_background = false;
       cpu_bottom = true;
       base_10_sizes = true;
