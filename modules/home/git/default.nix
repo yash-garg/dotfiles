03e91999965c8e1cdb4a0f8850059c4b62a0ad9e
Yash-Garg <me@yashgarg.dev>
2025-04-24 10:13:40 +0530

    chore: remove useless config wrapper

--- modules/home/git/default.nix
+++ modules/home/git/default.nix
@@ -6,21 +6,19 @@
 with lib;
 with lib.${namespace};
 {
-  config = {
-    programs.git = enabled // {
-      ignores = [
-        "key.properties"
-        "keystore.properties"
-        "*.jks"
-        ".direnv/"
-        ".DS_Store"
-        ".vscode/"
-        ".idea/"
-      ];
+  programs.git = enabled // {
+    ignores = [
+      "key.properties"
+      "keystore.properties"
+      "*.jks"
+      ".direnv/"
+      ".DS_Store"
+      ".vscode/"
+      ".idea/"
+    ];
 
-      includes = [
-        { path = snowfall.fs.get-file ".gitconfig"; }
-      ];
-    };
+    includes = [
+      { path = snowfall.fs.get-file ".gitconfig"; }
+    ];
   };
 }
