cacc7296f6deccb9d488377ef77b39d3e50d5a11
Yash-Garg <me@yashgarg.dev>
2025-04-29 11:31:46 +0530

    fix update caddy-tailscale sha

--- packages/caddy-tailscale/default.nix
+++ packages/caddy-tailscale/default.nix
@@ -3,5 +3,5 @@ caddy.withPlugins {
   plugins = [
     "github.com/tailscale/caddy-tailscale@v0.0.0-20250207163903-69a970c84556"
   ];
-  hash = "sha256-UR9CG/zIslkXHDj1fDWmhx8hJZ8VLvZzOTGvGqqx1Ls=";
+  hash = "sha256-USKNTAvxmuxzhqA8e8XERr1U8513ONG54Md5vcDUERg=";
 }
