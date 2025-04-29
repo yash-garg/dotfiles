908337bf6d6a0918e1ee86b05312829d78eac494
Yash-Garg <me@yashgarg.dev>
2025-04-29 11:30:58 +0530

    feat(zenith): add gatus service

--- systems/aarch64-linux/zenith/default.nix
+++ systems/aarch64-linux/zenith/default.nix
@@ -93,6 +93,82 @@ in
     };
   };
 
+  services.gatus = enabled // {
+    settings = {
+      web.port = 3333;
+      connectivity.checker = {
+        target = "1.1.1.1:53";
+        interval = "60s";
+      };
+      endpoints =
+        let
+          monitorPoints = [
+            {
+              name = "Actual Budget";
+              url = "https://vortex.${tailnet}:5006";
+            }
+            {
+              name = "Cadvisor";
+              url = "https://nova.turtle-lake.ts.net";
+            }
+            {
+              name = "Grafana";
+              url = "https://nova.${tailnet}:3000";
+            }
+            {
+              name = "Jellyfin";
+              url = "https://nova.${tailnet}:8096";
+            }
+            {
+              name = "Miniflux";
+              url = "https://nova.${tailnet}:5600";
+            }
+            {
+              name = "Minecraft Map";
+              url = "https://vortex.${tailnet}:81";
+            }
+            {
+              name = "Prometheus";
+              url = "https://nova.${tailnet}:9090";
+            }
+            {
+              name = "Plausible Analytics";
+              url = "https://plausible.${tailnet}";
+            }
+            {
+              name = "Prowlarr";
+              url = "https://nova.${tailnet}:9696";
+            }
+            {
+              name = "Radarr";
+              url = "https://nova.${tailnet}:7878";
+            }
+            {
+              name = "Sonarr";
+              url = "https://nova.${tailnet}:8989";
+            }
+            {
+              name = "qBittorrent";
+              url = "https://nova.${tailnet}:8080";
+            }
+            {
+              name = "unRAID";
+              url = "https://nova.${tailnet}";
+            }
+          ];
+          tailnet = config.services.tailscale.tailnet;
+        in
+        map (endpoint: {
+          inherit (endpoint) name url;
+          interval = "10m";
+          conditions = [
+            "[STATUS] == 200"
+            "[RESPONSE_TIME] < 300"
+          ];
+        }) monitorPoints;
+    };
+  };
+
   users.users.yash = {
     isNormalUser = true;
     hashedPasswordFile = config.age.secrets.user-password.path;
