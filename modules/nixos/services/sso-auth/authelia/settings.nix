74154bc6785790e74cc05c77d90406a074574d71
Yash-Garg <me@yashgarg.dev>
2025-05-31 19:17:29 +0530

    fix(authelia): use module import for settings

--- modules/nixos/services/sso-auth/authelia/settings.nix
+++ modules/nixos/services/sso-auth/authelia/settings.nix
@@ -11,230 +11,232 @@ let
   base_dn = "dc=${concatStringsSep ",dc=" (splitString "." domain)}";
 in
 {
-  default_2fa_method = "webauthn";
-  theme = "auto";
+  services.authelia.instances.main.settings = {
+    default_2fa_method = "webauthn";
+    theme = "auto";
 
-  access_control = {
-    default_policy = "deny";
-    networks = [
-      {
-        name = "local";
-        networks = [
-          "127.0.0.0/8"
-        ];
-      }
-    ];
-    rules = [
-      {
-        domain = "auth.${domain}";
-        policy = "two_factor";
-      }
-      {
-        domain = "*.${domain}";
-        policy = "one_factor";
-      }
-    ];
-  };
-
-  authentication_backend =
-    let
-      lldapEnabled = config.${namespace}.services.lldap.enable;
-    in
-    {
-      password_reset.disable = true;
-      refresh_interval = "5m";
-      file = mkIf (!lldapEnabled) {
-        inherit (config.age.secrets.usersFile) path;
-        password = {
-          algorithm = "argon2id";
-          iterations = 1;
-          key_length = 32;
-          salt_length = 16;
-          memory = 1024;
-          parallelism = 8;
-        };
-      };
-      ldap = mkIf lldapEnabled {
-        inherit base_dn;
-        address = "ldap://localhost:3890";
-        users_filter = "(&({username_attribute}={input})(objectClass=person))";
-        groups_filter = "(member={dn})";
-        user = "uid=admin,ou=people,${base_dn}";
-      };
-    };
-
-  identity_providers = {
-    oidc = {
-      enable_client_debug_messages = false;
-      enforce_pkce = "always";
-      lifespans = {
-        access_token = "30m";
-        authorize_code = "1m";
-        id_token = "30m";
-        refresh_token = "90m";
-      };
-      cors = {
-        endpoints = [
-          "authorization"
-          "token"
-          "revocation"
-          "introspection"
-          "userinfo"
-        ];
-        allowed_origins = [ "https://auth.${domain}" ];
-        allowed_origins_from_client_redirect_uris = true;
-      };
-      claims_policies.default = {
-        id_token = [
-          "email"
-          "email_verified"
-          "alt_emails"
-          "preferred_username"
-          "name"
-        ];
-      };
-      clients = [
+    access_control = {
+      default_policy = "deny";
+      networks = [
         {
-          client_id = "cloudflare";
-          client_name = "Cloudflare Access";
-          client_secret = "$pbkdf2-sha512$310000$0OVYTmffbZnm5QLF/aPQIA$AaOM/qZFPwy.f4sQSQW.NOsm302CovCx3osVs/C8bfTEOt1tpbsf.lSZqBCY97JBG5PAr/IuIJg2RS2rz/PT7g";
-          public = false;
-          authorization_policy = "two_factor";
-          claims_policy = "default";
-          consent_mode = "pre-configured";
-          pre_configured_consent_duration = "6M";
-          redirect_uris = [
-            "https://yashg.cloudflareaccess.com/cdn-cgi/access/callback"
+          name = "local";
+          networks = [
+            "127.0.0.0/8"
           ];
-          scopes = [
-            "openid"
-            "profile"
-            "email"
-          ];
-          userinfo_signed_response_alg = "RS256";
-          token_endpoint_auth_method = "client_secret_basic";
+        }
+      ];
+      rules = [
+        {
+          domain = "auth.${domain}";
+          policy = "two_factor";
         }
         {
-          client_id = "actual-budget";
-          client_name = "Actual Budget";
-          client_secret = "$pbkdf2-sha512$310000$iWomIaFYWHHBwj3ILVzL.Q$l9rRfes89uaXIGQnSZqtlLsAa8zMkjHWvUL39mnjHXBq.bokav5Z.dc3.mcUZxkW.5M64InDQZ5eg/81HWlETA";
-          public = false;
-          authorization_policy = "one_factor";
-          grant_types = [
-            "authorization_code"
-          ];
-          redirect_uris = [
-            "https://actual.${domain}/openid/callback"
-            "https://budget.${domain}/openid/callback"
-          ];
-          scopes = [
-            "openid"
-            "profile"
-            "groups"
-            "email"
-          ];
-          userinfo_signed_response_alg = "none";
-          token_endpoint_auth_method = "client_secret_basic";
-        }
-        {
-          client_id = "immich";
-          client_name = "Immich";
-          client_secret = "$pbkdf2-sha512$310000$we2.VRlN/pvtnZoUt0.kpw$qRAAKL..H4GnzEzMiMH.MPoXLy0IB3BslhB2.0gTVK99cuAyQEOsNNQ052huyqgpwdpTHVfaU68CmUzC.gnLGg";
-          public = false;
-          authorization_policy = "one_factor";
-          redirect_uris = [
-            "https://photos.${domain}/auth/login"
-            "https://photos.${domain}/user-settings"
-            "app.immich:///oauth-callback"
-          ];
-          scopes = [
-            "openid"
-            "profile"
-            "email"
-          ];
-          userinfo_signed_response_alg = "none";
-          token_endpoint_auth_method = "client_secret_post";
-        }
-        {
-          client_id = "jellyfin";
-          client_name = "Jellyfin";
-          client_secret = "$pbkdf2-sha512$310000$6MaPLvchHznyIyOpGM0pjw$wIBCSHF5R57zG9DtXyDn65jhPpkBL63/4PXh.MSYbiRxln.jg65OAF.E.cluk9ljSayfy1GemMYMAZG82JcwFg";
-          public = false;
-          authorization_policy = "one_factor";
-          require_pkce = true;
-          pkce_challenge_method = "S256";
-          redirect_uris = [
-            "https://stream.${domain}/sso/OID/redirect/authelia"
-          ];
-          scopes = [
-            "openid"
-            "profile"
-            "groups"
-          ];
-          userinfo_signed_response_alg = "none";
-          token_endpoint_auth_method = "client_secret_post";
+          domain = "*.${domain}";
+          policy = "one_factor";
         }
       ];
     };
-  };
 
-  log = {
-    level = "debug";
-    format = "json";
-    file_path = "/tmp/authelia.log";
-    keep_stdout = true;
-  };
+    authentication_backend =
+      let
+        lldapEnabled = config.${namespace}.services.lldap.enable;
+      in
+      {
+        password_reset.disable = true;
+        refresh_interval = "5m";
+        file = mkIf (!lldapEnabled) {
+          inherit (config.age.secrets.usersFile) path;
+          password = {
+            algorithm = "argon2id";
+            iterations = 1;
+            key_length = 32;
+            salt_length = 16;
+            memory = 1024;
+            parallelism = 8;
+          };
+        };
+        ldap = mkIf lldapEnabled {
+          inherit base_dn;
+          address = "ldap://localhost:3890";
+          users_filter = "(&({username_attribute}={input})(objectClass=person))";
+          groups_filter = "(member={dn})";
+          user = "uid=admin,ou=people,${base_dn}";
+        };
+      };
 
-  ntp = {
-    address = "time.cloudflare.com:123";
-    disable_startup_check = false;
-    disable_failure = true;
-    version = 4;
-    max_desync = "3s";
-  };
+    identity_providers = {
+      oidc = {
+        enable_client_debug_messages = false;
+        enforce_pkce = "always";
+        lifespans = {
+          access_token = "30m";
+          authorize_code = "1m";
+          id_token = "30m";
+          refresh_token = "90m";
+        };
+        cors = {
+          endpoints = [
+            "authorization"
+            "token"
+            "revocation"
+            "introspection"
+            "userinfo"
+          ];
+          allowed_origins = [ "https://auth.${domain}" ];
+          allowed_origins_from_client_redirect_uris = true;
+        };
+        claims_policies.default = {
+          id_token = [
+            "email"
+            "email_verified"
+            "alt_emails"
+            "preferred_username"
+            "name"
+          ];
+        };
+        clients = [
+          {
+            client_id = "cloudflare";
+            client_name = "Cloudflare Access";
+            client_secret = "$pbkdf2-sha512$310000$0OVYTmffbZnm5QLF/aPQIA$AaOM/qZFPwy.f4sQSQW.NOsm302CovCx3osVs/C8bfTEOt1tpbsf.lSZqBCY97JBG5PAr/IuIJg2RS2rz/PT7g";
+            public = false;
+            authorization_policy = "two_factor";
+            claims_policy = "default";
+            consent_mode = "pre-configured";
+            pre_configured_consent_duration = "6M";
+            redirect_uris = [
+              "https://yashg.cloudflareaccess.com/cdn-cgi/access/callback"
+            ];
+            scopes = [
+              "openid"
+              "profile"
+              "email"
+            ];
+            userinfo_signed_response_alg = "RS256";
+            token_endpoint_auth_method = "client_secret_basic";
+          }
+          {
+            client_id = "actual-budget";
+            client_name = "Actual Budget";
+            client_secret = "$pbkdf2-sha512$310000$iWomIaFYWHHBwj3ILVzL.Q$l9rRfes89uaXIGQnSZqtlLsAa8zMkjHWvUL39mnjHXBq.bokav5Z.dc3.mcUZxkW.5M64InDQZ5eg/81HWlETA";
+            public = false;
+            authorization_policy = "one_factor";
+            grant_types = [
+              "authorization_code"
+            ];
+            redirect_uris = [
+              "https://actual.${domain}/openid/callback"
+              "https://budget.${domain}/openid/callback"
+            ];
+            scopes = [
+              "openid"
+              "profile"
+              "groups"
+              "email"
+            ];
+            userinfo_signed_response_alg = "none";
+            token_endpoint_auth_method = "client_secret_basic";
+          }
+          {
+            client_id = "immich";
+            client_name = "Immich";
+            client_secret = "$pbkdf2-sha512$310000$we2.VRlN/pvtnZoUt0.kpw$qRAAKL..H4GnzEzMiMH.MPoXLy0IB3BslhB2.0gTVK99cuAyQEOsNNQ052huyqgpwdpTHVfaU68CmUzC.gnLGg";
+            public = false;
+            authorization_policy = "one_factor";
+            redirect_uris = [
+              "https://photos.${domain}/auth/login"
+              "https://photos.${domain}/user-settings"
+              "app.immich:///oauth-callback"
+            ];
+            scopes = [
+              "openid"
+              "profile"
+              "email"
+            ];
+            userinfo_signed_response_alg = "none";
+            token_endpoint_auth_method = "client_secret_post";
+          }
+          {
+            client_id = "jellyfin";
+            client_name = "Jellyfin";
+            client_secret = "$pbkdf2-sha512$310000$6MaPLvchHznyIyOpGM0pjw$wIBCSHF5R57zG9DtXyDn65jhPpkBL63/4PXh.MSYbiRxln.jg65OAF.E.cluk9ljSayfy1GemMYMAZG82JcwFg";
+            public = false;
+            authorization_policy = "one_factor";
+            require_pkce = true;
+            pkce_challenge_method = "S256";
+            redirect_uris = [
+              "https://stream.${domain}/sso/OID/redirect/authelia"
+            ];
+            scopes = [
+              "openid"
+              "profile"
+              "groups"
+            ];
+            userinfo_signed_response_alg = "none";
+            token_endpoint_auth_method = "client_secret_post";
+          }
+        ];
+      };
+    };
 
-  regulation = {
-    max_retries = "5";
-    find_time = "2m";
-    ban_time = "5m";
-  };
+    log = {
+      level = "debug";
+      format = "json";
+      file_path = "/tmp/authelia.log";
+      keep_stdout = true;
+    };
 
-  storage.postgres = {
-    address = "unix:///run/postgresql";
-    database = "authelia-main";
-    username = "authelia-main";
-    password = "authelia-main";
-  };
+    ntp = {
+      address = "time.cloudflare.com:123";
+      disable_startup_check = false;
+      disable_failure = true;
+      version = 4;
+      max_desync = "3s";
+    };
 
-  server = {
-    disable_healthcheck = true;
-    endpoints.authz.forward-auth.implementation = "ForwardAuth";
-  };
+    regulation = {
+      max_retries = "5";
+      find_time = "2m";
+      ban_time = "5m";
+    };
 
-  session.cookies = [
-    {
-      inherit domain;
-      authelia_url = "https://auth.${domain}";
-      default_redirection_url = "https://${domain}/";
-    }
-  ];
+    storage.postgres = {
+      address = "unix:///run/postgresql";
+      database = "authelia-main";
+      username = "authelia-main";
+      password = "authelia-main";
+    };
 
-  telemetry.metrics = {
-    enabled = true;
-  };
+    server = {
+      disable_healthcheck = true;
+      endpoints.authz.forward-auth.implementation = "ForwardAuth";
+    };
 
-  totp = {
-    disable = false;
-    issuer = domain;
-    disable_reuse_security_policy = true;
-  };
+    session.cookies = [
+      {
+        inherit domain;
+        authelia_url = "https://auth.${domain}";
+        default_redirection_url = "https://${domain}/";
+      }
+    ];
 
-  webauthn = {
-    disable = false;
-    enable_passkey_login = true;
-    display_name = "Authelia";
-    attestation_conveyance_preference = "direct";
-    selection_criteria.user_verification = "preferred";
-    timeout = "60s";
+    telemetry.metrics = {
+      enabled = true;
+    };
+
+    totp = {
+      disable = false;
+      issuer = domain;
+      disable_reuse_security_policy = true;
+    };
+
+    webauthn = {
+      disable = false;
+      enable_passkey_login = true;
+      display_name = "Authelia";
+      attestation_conveyance_preference = "direct";
+      selection_criteria.user_verification = "preferred";
+      timeout = "60s";
+    };
   };
 }
