e0e891443ac16691eff6489c2d16685a3a2f909d
Yash-Garg <me@yashgarg.dev>
2025-08-26 02:06:21 +0530

    chore: add actual-budget restic backup

--- modules/nixos/services/restic/default.nix
+++ modules/nixos/services/restic/default.nix
@@ -9,6 +9,7 @@ with lib;
 with lib.${namespace};
 let
   cfg = config.${namespace}.services.restic;
+  srv = config.services;
   r2_url = "06a4a54ded73aeb04fb12c679a65ed78.r2.cloudflarestorage.com";
   defaults = {
     initialize = true;
@@ -50,10 +51,21 @@ in
     };
 
     services.restic.backups = {
-      postgresql = defaults // {
-        paths = [ "${config.services.postgresqlBackup.location}/all.sql" ];
-        repository = "s3:${r2_url}/postgresql";
-      };
+      actual-budget = mkIf srv.actual.enable (
+        defaults
+        // {
+          paths = [ srv.actual.settings.dataDir ];
+          repository = "s3:${r2_url}/actual-budget";
+        }
+      );
+
+      postgresql = mkIf srv.postgresql.enable (
+        defaults
+        // {
+          paths = [ "${srv.postgresqlBackup.location}/all.sql" ];
+          repository = "s3:${r2_url}/postgresql";
+        }
+      );
     };
   };
 }
