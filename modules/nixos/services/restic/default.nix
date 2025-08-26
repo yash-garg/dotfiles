bcfaa802639f54728a94f37ca31353133c6e7432
Yash-Garg <me@yashgarg.dev>
2025-08-26 23:17:00 +0530

    chore: add a post backup notify hook

--- modules/nixos/services/restic/default.nix
+++ modules/nixos/services/restic/default.nix
@@ -33,6 +33,33 @@ let
       Persistent = true;
     };
   };
+  post-hook = app: ''
+    if [ $EXIT_STATUS -ne 0 ]; then
+      ${pkgs.curl}/bin/curl -H "Content-Type: application/json" \
+        -X POST \
+        -d "{\"content\": \"❌ Backup **${app}** failed!\"}" \
+        "$DISCORD_WEBHOOK"
+
+      ${pkgs.curl}/bin/curl -H "Content-Type: text/plain" \
+        -H "X-Title: Backup ${app}" \
+        -H "X-Priority: 5" \
+        -H "X-Tags: x,warning,backup" \
+        -d "Backup ${app} failed!" \
+        "$NTFY_URL"
+    else
+      ${pkgs.curl}/bin/curl -H "Content-Type: application/json" \
+        -X POST \
+        -d "{\"content\": \"✅ Backup **${app}** successful!\"}" \
+        "$DISCORD_WEBHOOK"
+
+      ${pkgs.curl}/bin/curl -H "Content-Type: text/plain" \
+        -H "X-Title: Backup ${app}" \
+        -H "X-Priority: 3" \
+        -H "X-Tags: white_check_mark,backup" \
+        -d "Backup ${app} successful!" \
+        "$NTFY_URL"
+    fi
+  '';
 in
 {
   options.${namespace}.services.restic = {
@@ -54,6 +81,7 @@ in
       actual-budget = mkIf srv.actual.enable (
         defaults
         // {
+          backupCleanupCommand = post-hook "actual-budget";
           paths = [
             srv.actual.settings.serverFiles
             srv.actual.settings.userFiles
@@ -65,6 +93,7 @@ in
       postgresql = mkIf srv.postgresql.enable (
         defaults
         // {
+          backupCleanupCommand = post-hook "postgresql";
           paths = [ "${srv.postgresqlBackup.location}/all.sql" ];
           repository = "s3:${r2_url}/postgresql";
         }
