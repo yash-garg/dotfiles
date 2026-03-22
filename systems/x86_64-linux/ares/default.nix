3f53d4e8a3793e4910ca630985ef5fdce2c4e709
Yash-Garg <me@yashgarg.dev>
2026-03-23 03:07:41 +0530

    quasar: update public ipv6 addr

--- systems/x86_64-linux/ares/default.nix
+++ systems/x86_64-linux/ares/default.nix
@@ -173,7 +173,7 @@ in
             ];
           };
           quasar = {
-            address = "http://[2405:201:4019:7033:5054:ff:fed0:31f1]:${toString ports.caddy}";
+            address = "http://[2405:201:4019:7033:cb7f:5f0e:7136:11cd]:${toString ports.caddy}";
             fallback = "http://[2a0c:9a40:8912::1]:${toString ports.caddy}";
             hosts = [
               "photos.ipx.ovh"
