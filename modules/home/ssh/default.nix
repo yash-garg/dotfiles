8a6c76e14d969b8bfc498fd4f409f2d9a207bc3f
Yash-Garg <me@yashgarg.dev>
2026-07-14 17:40:06 +0530

    authelia: update smtp settings

--- modules/home/ssh/default.nix
+++ modules/home/ssh/default.nix
@@ -15,19 +15,18 @@ with lib.${namespace};
       "~/.orbstack/ssh/config"
     ];
     settings = {
-      "*" =
-        {
-          AddKeysToAgent = "yes";
-          ServerAliveInterval = 60;
-          SendEnv = [ "COLORTERM" ];
-          SetEnv = {
-            TERM = "xterm-256color";
-          };
-        }
-        // optionalAttrs pkgs.stdenv.isDarwin {
-          IgnoreUnknown = "UseKeychain";
-          UseKeychain = "yes";
+      "*" = {
+        AddKeysToAgent = "yes";
+        ServerAliveInterval = 60;
+        SendEnv = [ "COLORTERM" ];
+        SetEnv = {
+          TERM = "xterm-256color";
         };
+      }
+      // optionalAttrs pkgs.stdenv.isDarwin {
+        IgnoreUnknown = "UseKeychain";
+        UseKeychain = "yes";
+      };
       "github.com".IdentityFile = "~/.ssh/git-ssh";
     };
   };
